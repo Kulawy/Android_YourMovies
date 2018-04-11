82ba2d073bb08fb7db77cfbe64dbab5bb9fad1bb
Kulawy <catatu1992@gmail.com>
2018-04-12 00:18:07 +0200

    TO DO: Fragments(actors, images), tests

--- app/src/main/java/com/pwr190729/jakubg/android_yourmovies/MovieDataServer.java
+++ app/src/main/java/com/pwr190729/jakubg/android_yourmovies/MovieDataServer.java
@@ -7,16 +7,19 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 
 /**
  * Created by JakubG on 04.04.2018.
  */
 
-public class MovieFiller {
+public class MovieDataServer {
 
+    private static MovieDataServer movieDataServer = null;
     private Context context;
     private ArrayList<Movie> moviesArrayList;
+    private HashMap< Integer, Movie> movieHashMap;
     //Resources res =
     private final int[] moviesArrays = { R.array.movie00, R.array.movie01, R.array.movie02, R.array.movie03, R.array.movie04, R.array.movie05, R.array.movie06, R.array.movie07, R.array.movie08, R.array.movie09};
     private final int[] actorsArrays = { R.array.actors00, R.array.actors01, R.array.actors02, R.array.actors03, R.array.actors04, R.array.actors05, R.array.actors06, R.array.actors07, R.array.actors08, R.array.actors09};
@@ -24,13 +27,38 @@ public class MovieFiller {
     private ArrayList<Integer> imagesList;
     Random rnd = new Random();
 
-    public MovieFiller(Context current, ArrayList<Movie> list){
-        moviesArrayList = list;
+    private MovieDataServer(Context current){
+        moviesArrayList = new ArrayList<Movie>();
+        movieHashMap = new HashMap<>();
         imagesList = new ArrayList<Integer>();
         this.context = current;
+        fillTheList();
     }
 
-    public ArrayList<Movie> fillTheList(){
+    public static MovieDataServer getInstance(Context context){
+        if (movieDataServer == null){
+            movieDataServer = new MovieDataServer(context);
+        }
+        return movieDataServer;
+    }
+
+    public ArrayList<Movie> getList(){
+        return moviesArrayList;
+    }
+
+    public HashMap<Integer, Movie> getMovieHashMap() {
+        return movieHashMap;
+    }
+
+    public Movie getMovieList(int position){
+        return moviesArrayList.get(position);
+    }
+
+    public Movie getMovieMap(int key){
+        return movieHashMap.get(key);
+    }
+
+    private void fillTheList(){
 
         for (int i = 0; i < moviesArrays.length; i++){
 
@@ -40,13 +68,15 @@ public class MovieFiller {
             for (int j = 0; j < 6; j++){
                 imagesList.add(iconImagesId[rnd.nextInt(8)]);
             }
-            String[] stingArray = context.getResources().getStringArray(moviesArrays[i]) ;
-            moviesArrayList.add( new Movie( stingArray[0], stingArray[1], decodeBitmapFromResource( context.getResources(), iconImagesId[i], 100, 100), imagesList, actorsArrays[i]));
+            String[] stringArray = context.getResources().getStringArray(moviesArrays[i]) ;
+            Movie newMovie = new Movie( i, stringArray[0], stringArray[1], decodeBitmapFromResource( context.getResources(), iconImagesId[i], 100, 100), imagesList, actorsArrays[i]);
+            moviesArrayList.add( newMovie );
+            movieHashMap.put( i, newMovie);
         }
 
-        return moviesArrayList;
     }
 
+    //2 metody dzięki którym ładowane są zmieniszone już bitmapy
     private Bitmap decodeBitmapFromResource(Resources res, int resId, int reqWidth, int reqHeight){
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inJustDecodeBounds = true;
